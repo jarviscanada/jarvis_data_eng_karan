7f1c196aa13b77116de8dd449adfc8eee056ead3
karan <karan.khalsa19@gmail.com>
2020-05-11 21:44:21 +0000

    psql_docker script updates from PR2

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #Setup arguments
+script_execute=$1
+PGUSER=$2
+PGPASSWORD=$3
 
 #Main code
-#check if docker_deamon is running - otherwise start run
+#check if docker_deamon is running - otherwise start
 systemctl status docker || systemctl start docker
 
 #check if user wants to create new psql table
@@ -12,31 +15,30 @@ if [ $1 == "create" ]; then
     echo "Error: Container already exists"
     exit 1
 
-  elif [ "$#" != 4 ]; then
+  elif [ "$#" != 3 ]; then
     echo "Error: database username or password not passed"
     exit 1
 
   else
     docker pull postgres
     docker volume create pgdata
-    export db_user=$2
-    export db_pass=$3
-    docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_pass \
-    -e POSTGRES_USER=$db_user -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+
+    docker run --name jrvs-psql -e POSTGRES_PASSWORD=$PGPASSWORD \
+    -e POSTGRES_USER=$PGUSER -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
     exit $?
     fi
 
-elif  [ "$(docker ps -a -f name=jrvs-psql | wc -l)" != "2" ]; then
-    echo "Error: Container not created"
-    exit 1
-
 else
+  if [ "$(docker ps -a -f name=jrvs-psql | wc -l)" != "2" ]; then
+    echo "Error: Container does not exist"
+    exit 1
+    fi
+
   if [ $1 == "start" ]; then
     docker container start jrvs-psql
     exit $?
 
   elif [ $1 == "stop" ]; then
-    docker container start jrvs-psql
     docker container stop jrvs-psql
     exit $?
 
@@ -44,4 +46,4 @@ else
     echo "Error: Invlaid argument"
     exit 1
     fi
-fi
+  fi
